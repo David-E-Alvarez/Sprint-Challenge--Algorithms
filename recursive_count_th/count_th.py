4677f8a69af96b281c0885ad0daa81fccd116b99
David-E-Alvarez <davidealvarez1@gmail.com>
2020-07-24 16:39:17 -0700

    comming up on finishing count_th

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,10 +6,17 @@ Your function must utilize recursion. It cannot contain any loops.
 def count_th(word):
     # TBC
     #test case #1:
+    th_str = "th"
+    th_len = len("th")
     if len(word) == 0:#base case
         return 0
     #else: #recursive case
-        #if "th" add 1
+        #check to see if first two letters are t and h; "th"
+        if word[0:th_len] == th_str:
+            #if so return rest of string without the first letter to
+            # check for first two letters again and see if they are "th"
+            return
+    
 
         
         
